cf2d83f431f52bba504122c176c1ced54128ee87
Himank Barve <himank.barve@squizify.com>
2025-12-08 02:21:14 +0530

    Comment out service and persistence settings in Qdrant Helm release configuration for future adjustments.

--- terraform/modules/databases/qdrant/main.tf
+++ terraform/modules/databases/qdrant/main.tf
@@ -5,16 +5,16 @@ resource "helm_release" "qdrant" {
   version    = "1.16.1"
   namespace  = var.namespace
 
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
-  set {
-    name  = "persistence.enabled"
-    value = "true"
-  }
-  set {
-    name  = "persistence.size"
-    value = "10Gi"
-  }
+  # set {
+  #   name  = "service.type"
+  #   value = "ClusterIP"
+  # }
+  # set {
+  #   name  = "persistence.enabled"
+  #   value = "true"
+  # }
+  # set {
+  #   name  = "persistence.size"
+  #   value = "10Gi"
+  # }
 }
